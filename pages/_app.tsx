564eae36c241c5b3d7462f715d33feb88210572a
brotherSam <samuelobior@yahoo.co.uk>
2023-07-01 14:01:51 +0100

    final final mainnet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ import "@/styles/style.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain="Binance Smart Chain">
+    <ThirdwebProvider activeChain="binance">
       <Component {...pageProps} />
       <Toaster />
     </ThirdwebProvider>
